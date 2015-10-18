a96251488c3ad0e9d392d0b0e07278d31ee33a74
Il Jae Lee <iljae@umich.edu>
2015-10-17 22:27:40 -0400

    Added data collection and SVM test methods.

---
+++ collect_data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+Collects data from a video source.
+"""
+
+import cv2
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a HOG classifier.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("width", metavar="W", type=int, help="width of the HOG window")
+    parser.add_argument("height", metavar="H", type=int, help="height of the HOG window")
+    parser.add_argument("output", metavar="O", type=str, help="path to save the collected data")
+    parser.add_argument("-s", "--source", nargs="?", type=int, default=0, help="camera source")
+    parser.add_argument("-m", "--max", nargs="?", type=int, default=40, help="max datasets to collect (0 is unlimited)")
+
+    args = parser.parse_args()
+
+    width = args.width
+    height = args.height
+    output_dir = args.output
+    source = args.source
+    max = args.max
+
+    SIZE = (width, height)
+
+    video_capture = cv2.VideoCapture(source)
+
+    video_capture.set(3,width)
+    video_capture.set(4,height)
+
+    record = False
+    frames_count = 0
+    total_frames_count = 0
+    while True:
+        ret, frame = video_capture.read()
+
+        if record:
+            if frames_count < max:
+                cv2.imwrite(output_dir+"/"+str(total_frames_count)+".png", frame)
+                frames_count += 1
+                total_frames_count += 1
+                print str(total_frames_count) + " framed recorded..."
+            else:
+                record = False
+
+        cv2.imshow("Video", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        elif key == 32:
+            record = True
+            frames_count = 0
+
+    video_capture.release()
+    cv2.destroyAllWindows()
---
+++ test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+Tests the trained data.
+"""
+
+import cv2
+import os
+import argparse
+
+def draw_detections(img, rects):
+    for x, y, w, h in rects:
+        # the HOG detector returns slightly larger rectangles than the real objects.
+        # so we slightly shrink the rectangles to get a nicer output.
+        pad_w, pad_h = int(0.15*w), int(0.05*h)
+        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), 3)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a HOG classifier.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("width", metavar="W", type=int, help="width of the HOG window")
+    parser.add_argument("height", metavar="H", type=int, help="height of the HOG window")
+    parser.add_argument("-o", "--output", nargs="?", type=str, default=os.getcwd()+"/result.model", help="detecting vector output file")
+    parser.add_argument("-s", "--source", nargs="?", type=int, default=0, help="camera source")
+
+    args = parser.parse_args()
+
+    width = args.width
+    height = args.height
+    output_dir = args.output
+    source = args.source
+
+    SIZE = (width, height)
+
+    hog = cv2.HOGDescriptor()
+    hog.winSize = SIZE
+
+    video_capture = cv2.VideoCapture(source)
+
+    while True:
+        ret, frame = video_capture.read()
+
+        found, w = hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
+        draw_detections(frame, found)
+
+        cv2.imshow("Video", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
