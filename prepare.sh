520222a16a89bf37bc094582ef7fffe58b28db82
Il Jae Lee <iljae@umich.edu>
2015-12-04 17:10:48 -0500

    Image orientation is now fixed.

--- prepare.sh
+++ prepare.sh
@@ -9,10 +9,8 @@ for FILE in $SOURCE_PATH/*.$EXT;
 do
     echo $FILE
     FILENAME=$(basename $FILE .$EXT)
-    cp $FILE $OUTPUT_PATH/$FILENAME.$EXT
-    convert $FILE -flip $OUTPUT_PATH/$FILENAME\_f.$EXT
-    convert $FILE -motion-blur 0x12+200 $OUTPUT_PATH/$FILENAME\_b.$EXT
-    convert $FILE -motion-blur 0x12-200 $OUTPUT_PATH/$FILENAME\_b.$EXT
-    convert $FILE -motion-blur 0x12+200 -flip $OUTPUT_PATH/$FILENAME\_bf.$EXT
-    convert $FILE -motion-blur 0x12-200 -flip $OUTPUT_PATH/$FILENAME\_bf.$EXT
+    convert $FILE -auto-orient $OUTPUT_PATH/$FILENAME.$EXT
+    convert $FILE -auto-orient -flop $OUTPUT_PATH/$FILENAME\_f.$EXT
+    # convert $FILE -auto-orient -motion-blur 20x10+45 $OUTPUT_PATH/$FILENAME\_b.$EXT
+    # convert $FILE -auto-orient -motion-blur 20x10+45 -flop $OUTPUT_PATH/$FILENAME\_bf.$EXT
 done
