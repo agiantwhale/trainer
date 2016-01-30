0b10dd7031d1b1ef4e9099b64ba906ce615e2298
Il Jae Lee <iljae@umich.edu>
2016-01-30 12:13:31 -0500

    Use radial blur instead of motion blur to prepare blurred images.

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-SOURCE_PATH=""
-OUTPUT_PATH=""
+SOURCE_PATH="/Users/iljae/Development/nav-train/roomba/train/positive"
+OUTPUT_PATH="/Users/iljae/Development/nav-train/roomba/train/positive_sel"
 EXT="JPG"
 
 for FILE in $SOURCE_PATH/*.$EXT;
@@ -11,6 +11,6 @@ do
     FILENAME=$(basename $FILE .$EXT)
     convert $FILE -auto-orient $OUTPUT_PATH/$FILENAME.$EXT
     convert $FILE -auto-orient -flop $OUTPUT_PATH/$FILENAME\_f.$EXT
-    # convert $FILE -auto-orient -motion-blur 20x10+45 $OUTPUT_PATH/$FILENAME\_b.$EXT
-    # convert $FILE -auto-orient -motion-blur 20x10+45 -flop $OUTPUT_PATH/$FILENAME\_bf.$EXT
+    convert $FILE -auto-orient -radial-blur 8 $OUTPUT_PATH/$FILENAME\_b.$EXT
+    convert $FILE -auto-orient -radial-blur 8 -flop $OUTPUT_PATH/$FILENAME\_bf.$EXT
 done
